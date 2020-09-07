fe772b58b637f67c85e8cafd63bb816438bd786b
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-09-08 11:55:59 +1200

    Update config.php

--- site_template/config.php
+++ site_template/config.php
@@ -2,9 +2,9 @@
 
 ini_set('display_errors', 'on'); // change to 'off when live
 
-define ("DB_NAME", "00_l2_games");
-define ("DB_USERNAME", "root");
-define ("DB_PASSWORD", "");
+define ("DB_NAME", "browner71160");
+define ("DB_USERNAME", "_browner71160");
+define ("DB_PASSWORD", "f431f088a36e05a95f2b3b1f9fc081b5");
 define ("DB_HOST", "localhost");
 
 ?>
