eaf32d0bf51e25fbfe779c026b2392fa64b404b1
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-09-08 12:17:48 +1200

    Update index.php

--- site_template/index.php
+++ site_template/index.php
@@ -12,21 +12,4 @@
             
         </div> <!-- / main -->
         
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
-
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC GTT 20XX
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+<?php include("bottombit.php") ?>  
